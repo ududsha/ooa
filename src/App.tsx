fe8b0f9371edc33c7b9248fcdb47ede4db0f9b11
Shamendra Siri <shamendra@pomelopay.com>
2022-07-10 10:28:06 +0530

    Name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div>Organizarion Onboarding App - 1Centre!</div>;
+  return <div>Organizarion Onboarding App!</div>;
 }
 
 export default App;
